dfbb05e865074b9e66ab3b2b30ef0931492ea31e
biyin <854105610@qq.com>
2023-12-07 15:44:03 +0800

    feat: vue3+ts项目创建

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import LeftHome from './components/LeftHome.vue'
+import './style.css'
+</script>
+
+<template>
+  <div class="app">
+    <LeftHome />
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
+
---
+++ src/components/LeftHome.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div style="min-width: 320px; background-color: antiquewhite; min-height: 100%;">
+    12342345
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
